68420825f047f110e67fae8e21f77a84b0c24600
Piotr Bibrzycki <bibrzycki@gmail.com>
2019-07-28 20:09:03 +0200

    Game 3 - is that the number i think about?

--- src/pl/coderslab/game3/Main1.java
+++ src/pl/coderslab/game3/Main1.java
@@ -1,4 +1,61 @@
 package pl.coderslab.game3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main1 {
+    public static void main(String[] args) {
+
+        System.out.println("Pomyśl liczbę 0 do 1000, a ja ją zgadnę w max. 10 próbach");
+
+        int min = 0;
+
+        int max = 1000;
+
+        int numbersOfTry = 0;
+
+        while (true) {
+
+            int newRandom = randomNumber(min, max);
+            Scanner scan = new Scanner(System.in);
+
+            String tekst = scan.nextLine();
+
+            if (tekst.equals("zgadłeś")) {
+
+                ++numbersOfTry;
+                System.out.println("Wygrałem! Udało mi się odgadnąć w " + numbersOfTry + " próbach :-)");
+                break;
+
+            } else if (tekst.equals("za mało")) {
+
+                ++numbersOfTry;
+                min = newRandom + 1;
+
+            } else if (tekst.equals("za dużo")) {
+
+                ++numbersOfTry;
+                max = newRandom - 1;
+
+            } else {
+
+                System.out.println("nie rozpoznałem słowa, więc losuję ponownie");
+
+            }
+
+        }
+    }
+
+    public static int randomNumber(int min, int max) {
+
+        Random random = new Random();
+
+        int newRandomNumber = random.nextInt(max + 1 - min) + min;
+
+        System.out.println("Zgaduję: " + newRandomNumber + ". Czy trafiłem? (odpowiedz: \"za dużo\", \"za mało\", \"zgadłeś\")");
+
+        return newRandomNumber;
+
+    }
+
 }
